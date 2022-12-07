56803968da51d10a2bf20c35f60bd41f5de50e4e
Emik <emik.lakunishok@gmail.com>
2022-12-07 05:47:57 +0100

    Update build.sh

--- Rust/rhainterop/build.sh
+++ Rust/rhainterop/build.sh
@@ -1,11 +1,4 @@
-rustup target add aarch64-unknown-linux-gnu
-rustup target add i686-pc-windows-gnu
-rustup target add i686-pc-windows-msvc
-rustup target add i686-unknown-linux-gnu
-rustup target add x86_64-apple-darwin
-rustup target add x86_64-pc-windows-gnu
-rustup target add x86_64-pc-windows-msvc
-rustup target add x86_64-unknown-linux-gnu
+rustup target add --toolchain nightly aarch64-unknown-linux-gnu i686-pc-windows-gnu i686-pc-windows-msvc i686-unknown-linux-gnu x86_64-apple-darwin x86_64-pc-windows-gnu x86_64-pc-windows-msvc x86_64-unknown-linux-gnu
 cargo build --target=aarch64-unknown-linux-gnu --release
 cargo build --target=i686-pc-windows-gnu --release
 cargo build --target=i686-pc-windows-msvc --release
