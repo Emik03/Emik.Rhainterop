7ba9cbc54d748bac1d621d18ef5dd029287197c2
Emik <emik.lakunishok@gmail.com>
2022-12-22 14:53:50 +0100

    Add regions to GlobalUsings

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,5 +1,14 @@
-﻿// <copyright file="GlobalUsings.cs" company="Emik">
+﻿#region Emik.MPL
+
+// <copyright file="GlobalUsings.cs" company="Emik">
 // Copyright (c) Emik. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // </copyright>
+
+#endregion
+
+#region
+
 global using MessagePack;
 global using static Emik.Results.Please;
+
+#endregion
